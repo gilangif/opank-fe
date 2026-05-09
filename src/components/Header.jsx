2a8cdc05fd64f570f9b0307d71b0fd4c278f260b
gilangif <gilangidulfitri1201@gmail.com>
2026-05-10 01:23:14 +0700

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,69 @@
+import { Sheet } from "react-modal-sheet"
+import { useRef, useState } from "react"
+
+function SessionBox({ isOpen, setOpen }) {
+  const [text, setText] = useState("")
+
+  return (
+    <Sheet isOpen={isOpen} onClose={() => setOpen(false)} disableDrag={true} detent="content-height" avoidKeyboard={false}>
+      <Sheet.Backdrop onTap={() => setOpen(false)} className="!bg-black/20 backdrop-blur-sm" />
+      <Sheet.Container className="!bg-neutral-800 !text-white">
+        <Sheet.Header className="flex flex-row rounded-t-lg py-2">
+          <div className="flex flex-col justify-center items-start px-3">
+            <p className="m-0 p-0 text-sm font-bold">ADD SESSIONS</p>
+            <p className="m-0 p-0 text-[9px]">ALIPAYJSESSIONID</p>
+          </div>
+          <div className="flex flex-1 justify-end rounded-lg px-3 py-1 gap-6">
+            <div className="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 hover:text-green-500">
+                <path
+                  fillRule="evenodd"
+                  d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 hover:text-yellow-500">
+                <path
+                  fillRule="evenodd"
+                  d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+
+            <div className="flex items-center justify-center" onClick={() => setOpen(false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 hover:text-red-500">
+                <path
+                  fillRule="evenodd"
+                  d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+        </Sheet.Header>
+        <Sheet.Content className="p-2 border-0">
+          <textarea
+            placeholder="Paste sessions here..."
+            className="w-full h-[30vh] rounded-lg border border-neutral-600 bg-neutral-900 p-2 text-[0.7rem] text-white placeholder:text-white-500 outline-none focus:border-yellow-500"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </Sheet.Content>
+      </Sheet.Container>
+    </Sheet>
+  )
+}
+
 export default function Header() {
+  const [isOpen, setOpen] = useState(false)
+
   return (
     <>
+      <SessionBox isOpen={isOpen} setOpen={setOpen} />
       <div className="h-full">
         <nav className="after:pointer-events-none">
           <div className="flex items-center justify-between py-4 px-2">
@@ -19,7 +82,7 @@ export default function Header() {
             </div>
 
             <div className="flex gap-4 px-1">
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center" onClick={() => setOpen(true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-white hover:text-yellow-500 size-7">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
   return (
     <>
-      <div className="fixed bottom-0 left-0 w-full bg-neutral-800 p-2 py-3 shadow-xl shadow-stone-500 rounded-t-xl ">
-        <div className="flex justify-around items-center h-10">
+      <div className="fixed bottom-0 left-0 w-full bg-neutral-800 md:p-1 p-2 shadow-xl shadow-stone-500 rounded-t-xl ">
+        <div className="flex justify-around md:justify-start md:gap-6 items-center md:px-4">
           <a href="/" className="flex flex-col items-center text-white hover:text-yellow-500">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 md:size-4">
               <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
               <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
             </svg>
 
-            <span className="text-[10px] mt-1">Home</span>
+            <span className="text-[10px] mt-1 md:mt-0">Home</span>
           </a>
 
           <a href="/chats" className="flex flex-col items-center text-white hover:text-yellow-500">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 md:size-4">
               <path
                 fillRule="evenodd"
                 d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z"
@@ -23,19 +23,19 @@ export default function Navbar() {
               />
             </svg>
 
-            <span className="text-[10px] mt-1">Chat</span>
+            <span className="text-[10px] mt-1 md:mt-0">Chat</span>
           </a>
 
           <a href="/monitor" className="flex flex-col items-center text-white hover:text-yellow-500">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 md:size-4">
               <path d="M10.5 1.875a1.125 1.125 0 0 1 2.25 0v8.219c.517.162 1.02.382 1.5.659V3.375a1.125 1.125 0 0 1 2.25 0v10.937a4.505 4.505 0 0 0-3.25 2.373 8.963 8.963 0 0 1 4-.935A.75.75 0 0 0 18 15v-2.266a3.368 3.368 0 0 1 .988-2.37 1.125 1.125 0 0 1 1.591 1.59 1.118 1.118 0 0 0-.329.79v3.006h-.005a6 6 0 0 1-1.752 4.007l-1.736 1.736a6 6 0 0 1-4.242 1.757H10.5a7.5 7.5 0 0 1-7.5-7.5V6.375a1.125 1.125 0 0 1 2.25 0v5.519c.46-.452.965-.832 1.5-1.141V3.375a1.125 1.125 0 0 1 2.25 0v6.526c.495-.1.997-.151 1.5-.151V1.875Z" />
             </svg>
 
-            <span className="text-[10px] mt-1">Monitor</span>
+            <span className="text-[10px] mt-1 md:mt-0">Monitor</span>
           </a>
 
           <a href="/claims" className="flex flex-col items-center text-white hover:text-yellow-500">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 md:size-4">
               <path
                 fillRule="evenodd"
                 d="M3.792 2.938A49.069 49.069 0 0 1 12 2.25c2.797 0 5.54.236 8.209.688a1.857 1.857 0 0 1 1.541 1.836v1.044a3 3 0 0 1-.879 2.121l-6.182 6.182a1.5 1.5 0 0 0-.439 1.061v2.927a3 3 0 0 1-1.658 2.684l-1.757.878A.75.75 0 0 1 9.75 21v-5.818a1.5 1.5 0 0 0-.44-1.06L3.13 7.938a3 3 0 0 1-.879-2.121V4.774c0-.897.64-1.683 1.542-1.836Z"
@@ -43,7 +43,7 @@ export default function Navbar() {
               />
             </svg>
 
-            <span className="text-[10px] mt-1">Claims</span>
+            <span className="text-[10px] mt-1 md:mt-0">Claims</span>
           </a>
 
           <a href="/groups" className="flex flex-col items-center text-white hover:text-yellow-500">
@@ -56,7 +56,7 @@ export default function Navbar() {
               <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
             </svg>
 
-            <span className="text-[10px] mt-1">Group</span>
+            <span className="text-[10px] mt-1 md:mt-0">Group</span>
           </a>
         </div>
       </div>
